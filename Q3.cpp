bc30a1120549480065e4d21c4bd05995a533fed1
Hatim <Hatimdd@gmail.com>
2020-10-05 13:32:29 +0400

    initial commit

---
+++ Q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+    float x, y;
+    std::cout << "Please enter two numbers: ";
+    std::cin >> x >> y;
+    std::cout << "\nThe sum of " << x << " and " << y << " is " << x + y << std::endl;
+    std::cout << "The difference between " << x << " and " << y << " is " << x - y << std::endl;
+    std::cout << "The product of " << x << " and " << y << " is " << x * y << std::endl;
+    std::cout << "The distance between " << x << " and " << y << " is " <<(x > y ? abs(x)-abs(y) : abs(y)-abs(x))<< std::endl;
+    std::cout << "The mean average of " << x << " and " << y << " is " << (x + y)/2 << std::endl;
+}
